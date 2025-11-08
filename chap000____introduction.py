f92115a87a6cc46b74df4f1ba794b18bf6487482
gopichandblr02 <gopichand.blr02@gmail.com>
2025-11-07 22:04:12 -0600

    new adds

--- chap000____introduction.py
+++ chap000____introduction.py
@@ -5,6 +5,19 @@ def main():
 if __name__ == "__main__":
     main()
 
+# * When you run a program, OS loads your program into memory
+# * OS then call for a startup routine provided by the language runtime
+# * Startup routine looks for main() -  this is defined as starting point
+# * It is a standard convention
+
+# *  Every python file(module) has a built in variable called __name__
+# * If file is run directly -> then python sets __name__ == "__main__"
+# * If file is imported into another file (like import myScript) then  __name__ = "<module_name>"
+
+
+
+
+
 
 """
 Python is a widely used programming language for creating real-world applications. It is extensively used in:
@@ -16,6 +29,7 @@ Testing
 Backend Development
 Thus, learning Python offers significant advantages for your career opportunities.
 
-However, there are certain fields where Python doesn't excel. For example, if you are interested in frontend development, game development, or mobile app development, then Python is not the best choice.
+However, there are certain fields where Python doesn't excel. For example, if you are interested in frontend development,
+ game development, or mobile app development, then Python is not the best choice.
 
 """
